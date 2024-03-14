a52774407a5de6d2547443dac37a1eed7458869b
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-03-13 22:21:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,37 +3,52 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from io import BytesIO
 
-# Function to create a figure
-def create_figure():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Sample plot
-    return fig
+st.set_page_config(
+    page_title='Equipamentos - GCINFRA',
+    layout='wide',
+    page_icon="https://media.licdn.com/dms/image/C4D0BAQHXylmAyGyD3A/company-logo_200_200/0/1630570245289?e=2147483647&v=beta&t=Dxas2us5gteu0P_9mdkQBwJEyg2aoc215Vrk2phu7Bs",
+    initial_sidebar_state='auto'
+)
+url = "https://docs.google.com/spreadsheets/d/1CGypJWn35SFD6oYeLmFU1HjMhxJRBpuuVuVeP7y86wo/edit#gid=0"
+st.title("EQUIPAMENTOS - Engenharia Clínica")
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Function to convert figure to PDF bytes
-def fig_to_pdf_bytes(fig):
-    buf = BytesIO()
-    with PdfPages(buf) as pdf:
-        pdf.savefig(fig)
-    buf.seek(0)
-    return buf
 
-# Streamlit app
-def main():
-    st.title("Chart to PDF Example")
 
-    # Button to generate PDF
-    if st.button("Generate PDF"):
-        fig = create_figure()
-        pdf_bytes = fig_to_pdf_bytes(fig)
-        st.download_button(
-            label="Download Chart as PDF",
-            data=pdf_bytes,
-            file_name="chart.pdf",
-            mime="application/pdf"
-        )
 
-if __name__ == "__main__":
-    main()
+
+
+# # Function to create a figure
+# def create_figure():
+#     fig, ax = plt.subplots()
+#     ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Sample plot
+#     return fig
+
+# # Function to convert figure to PDF bytes
+# def fig_to_pdf_bytes(fig):
+#     buf = BytesIO()
+#     with PdfPages(buf) as pdf:
+#         pdf.savefig(fig)
+#     buf.seek(0)
+#     return buf
+
+# # Streamlit app
+# def main():
+#     st.title("Chart to PDF Example")
+
+#     # Button to generate PDF
+#     if st.button("Generate PDF"):
+#         fig = create_figure()
+#         pdf_bytes = fig_to_pdf_bytes(fig)
+#         st.download_button(
+#             label="Download Chart as PDF",
+#             data=pdf_bytes,
+#             file_name="chart.pdf",
+#             mime="application/pdf"
+#         )
+
+# if __name__ == "__main__":
+#     main()
 
 
 # example/st_app.py
@@ -41,9 +56,7 @@ if __name__ == "__main__":
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1GswNpQuhhc6udp59clV5s6dDnBfFF91rofaRbMsDdT0/edit?usp=sharing"
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url, usecols=[0, 10])
-st.dataframe(data)
+df = conn.read(spreadsheet=url, usecols=list(range(12)))
+st.dataframe(df)

a147da9aee2a6d762df88875c5929c0ccede9155
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-03-13 22:59:42 -0300

    Add files via upload

---
+++ generate_keys (1).py
@@ -0,0 +1,14 @@
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+names = ["Peter Parker", "Rebecca Miller", "Pedro Borges", "Arthur Pires", "Kaio Razotto", "Vitor Peixoto", "Claudemiro Dourado", "Izabela Lopes", "Fernando Souza", "Michelle Bonfim", "Gerson Rodrigues", "Mônica Guimarães", "Pedro Andrade", "Jéssyca de Paula", "Rafael Souza"]
+usernames = ["pparker", "rmiller", "pborges", "arthur.pires", "kaio.razotto", "vitor.peixoto", "claudemiro.dourado", "izabela.lopes", "fernando.souza", "michelle.bonfim", "gerson.rodrigues", "monica.guimaraes", "pedro.andrade", "jessyca.depaula", "rafael.souza"]
+passwords = ["XXX", "XXX", "agirabc", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024", "Agir@2024"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)

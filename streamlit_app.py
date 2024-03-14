5e7a6e85954d7311ff317edd4724f8bb892c15b3
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-03-13 22:25:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,48 +16,5 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 
 
-
-
-
-# # Function to create a figure
-# def create_figure():
-#     fig, ax = plt.subplots()
-#     ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Sample plot
-#     return fig
-
-# # Function to convert figure to PDF bytes
-# def fig_to_pdf_bytes(fig):
-#     buf = BytesIO()
-#     with PdfPages(buf) as pdf:
-#         pdf.savefig(fig)
-#     buf.seek(0)
-#     return buf
-
-# # Streamlit app
-# def main():
-#     st.title("Chart to PDF Example")
-
-#     # Button to generate PDF
-#     if st.button("Generate PDF"):
-#         fig = create_figure()
-#         pdf_bytes = fig_to_pdf_bytes(fig)
-#         st.download_button(
-#             label="Download Chart as PDF",
-#             data=pdf_bytes,
-#             file_name="chart.pdf",
-#             mime="application/pdf"
-#         )
-
-# if __name__ == "__main__":
-#     main()
-
-
-# example/st_app.py
-
-import streamlit as st
-from streamlit_gsheets import GSheetsConnection
-
-
-
 df = conn.read(spreadsheet=url, usecols=list(range(12)))
 st.dataframe(df)

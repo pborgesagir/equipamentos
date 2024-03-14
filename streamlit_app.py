e0a5cc04bc7c753a3e5717d9aa060fba88733536
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-03-13 23:03:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,27 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import streamlit.components.v1 as components
+import pygwalker as pyg
+from datetime import datetime
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import re
+import streamlit_authenticator as stauth 
+import pickle
+from pathlib import Path
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from io import BytesIO
+import base64
+import time
+from streamlit_carousel import carousel
+from reportlab.lib.utils import ImageReader
+import sys
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from io import BytesIO
-from streamlit_gsheets import GSheetsConnection
 
 # Set Streamlit page configuration
 st.set_page_config(
@@ -12,28 +31,55 @@ st.set_page_config(
     initial_sidebar_state='auto'
 )
 
-# Google Sheets URL
-url = "https://docs.google.com/spreadsheets/d/1GswNpQuhhc6udp59clV5s6dDnBfFF91rofaRbMsDdT0/edit#gid=704841034"
-# Centered title using HTML tags
-st.markdown("<h1 style='text-align: center;'>GESTÃO DE RECURSO PARA INVESTIMENTO</h1>", unsafe_allow_html=True)
+#testing authentication
+# --- USER AUTHENTICATION ---
 
 
-# Adding a centered subtitle with larger font size using HTML
-st.markdown("""
-    <div style='text-align: center; font-size: 36px;'>
-        <b>SOF - DCOL - GCINFRA</b>
-    </div>
-""", unsafe_allow_html=True)
+names = ["Peter Parker", "Rebecca Miller", "Pedro Borges", "Arthur Pires", "Kaio Razotto", "Vitor Peixoto", "Claudemiro Dourado", "Izabela Lopes", "Fernando Souza", "Michelle Bonfim", "Gerson Rodrigues", "Mônica Guimarães", "Pedro Andrade", "Jéssyca de Paula", "Rafael Souza"]
+usernames = ["pparker", "rmiller", "pborges", "arthur.pires", "kaio.razotto", "vitor.peixoto", "claudemiro.dourado", "izabela.lopes", "fernando.souza", "michelle.bonfim", "gerson.rodrigues", "monica.guimaraes", "pedro.andrade", "jessyca.depaula", "rafael.souza"]
 
-st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
 
-# st.sidebar.image('index.png', width=150)
-st.sidebar.title(f"Bem-vindo, {name}")
-conn = st.connection("gsheets", type=GSheetsConnection)
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
 
-df = conn.read(spreadsheet=url, worksheet="METRICAS", usecols=list(range(12)))
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=0.00694)
 
-# Display the DataFrame in Streamlit
-st.dataframe(df)
+name, authentication_status, username = authenticator.login("Login de acesso: Gestão de Recurso para Investimento - GCINFRA", "main")
+
+if authentication_status == False:
+    st.error("Usuário/senha está incorreto")
+
+if authentication_status == None:
+    st.warning("Por favor, insira usuário e senha")
+
+if authentication_status:
+
+
+    # Google Sheets URL
+    url = "https://docs.google.com/spreadsheets/d/1GswNpQuhhc6udp59clV5s6dDnBfFF91rofaRbMsDdT0/edit#gid=704841034"
+    # Centered title using HTML tags
+    st.markdown("<h1 style='text-align: center;'>EQUIPAMENTOS -  Engenharia Clínica</h1>", unsafe_allow_html=True)
+    
+    
+    # Adding a centered subtitle with larger font size using HTML
+    st.markdown("""
+        <div style='text-align: center; font-size: 36px;'>
+            <b>SOF - DCOL - GCINFRA</b>
+        </div>
+    """, unsafe_allow_html=True)
+    
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    
+    # st.sidebar.image('index.png', width=150)
+    st.sidebar.title(f"Bem-vindo, {name}")
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    
+    df = conn.read(spreadsheet=url, worksheet="METRICAS", usecols=list(range(12)))
+    
+    # Display the DataFrame in Streamlit
+    st.dataframe(df)
